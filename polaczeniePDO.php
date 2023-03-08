1bf97207e69d73f094518fc7db1cfa006147a395
Mateusz Kacpura <mateusz.kacpura@protonmail.com>
2023-03-08 10:28:55 +0100

    Add files via upload
    Aktualizacja do PHP 8.0, poprawki starych błędów, wprowadzam biblioteki PDO

--- polaczeniePDO.php
+++ polaczeniePDO.php
@@ -1,5 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=fiszki_nauka_slowek', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                            
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=fiszki_nauka_slowek', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                      
 ?>
