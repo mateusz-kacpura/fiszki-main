4d5200c49bee6412f9319e12066c675b7d30486e
Mateusz Kacpura <mateusz.kacpura@protonmail.com>
2023-03-08 22:20:04 +0100

    Add files via upload
    Migracja połączeń z bazą danych na PDO

--- polaczeniePDO.php
+++ polaczeniePDO.php
@@ -1,4 +1,17 @@
 <?php
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=fiszki_nauka_slowek', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                      
+
+$host = 'mysql:host=127.0.0.1; dbname=fiszki_nauka_slowek';
+$server = 'mysql:host=127.0.0.1';
+$databasename = 'fiszki_nauka_slowek';
+$login = 'root';
+$password = '';
+
+$option = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);        
+
+$pdo = new PDO($host, $login, $password, $option);
+$pdo_set = new PDO( $server, $login, $password );
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
 ?>
--- table_settings/load_table_fiszki.php
+++ table_settings/load_table_fiszki.php
@@ -1,17 +1,36 @@
 <?php
-include('../baza.php');
-if(!$connect){
-    header('location: ../index.php');
+
+require_once('../polaczeniePDO.php');
+
+if(!isset($_SESSION))      
+{         
+     session_start();      
 }
-if($_POST['table']!=''){
-$table=$_POST['table'];
-$query = "select * from $table";
-$result=mysqli_query($connect,$query);
- while ($row = mysqli_fetch_assoc($result)) {
-echo '<script>array1.push("'.$row['v1'].'");array2.push("'.$row['v2'].'");weight.push("'.$row['weight'].'")</script>';
+
+$flaga = 1;
+
+if (!isset($_POST['table']))
+{
+    $flaga = 0;
 }
+
+if ($flaga == 1)
+{
+    try
+    { 
+        $table = $_POST['table'];
+        $querty = "SELECT * FROM $table";
+        echo $querty."</br>";
+        $liczba = $pdo -> query($querty);
+        while ($row = $liczba->fetch()) 
+        {
+            echo '<script>array1.push("'.$row['v1'].'");array2.push("'.$row['v2'].'");weight.push("'.$row['weight'].'")</script>';
+        }
+    }
+    catch(PDOException $e)
+    {
+        echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
+    }
 }
-else{
-    header('location: ../fiszki.php');
-}
+
 ?>
