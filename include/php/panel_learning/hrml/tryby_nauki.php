0ad07e5b337eebec9f8cd318d524992e8408537e
nowybackup <s167708@student.pg.edu.pl>
2023-03-12 23:28:50 +0100

    podzial na pliki

---
+++ include/php/panel_learning/hrml/tryby_nauki.php
@@ -0,0 +1,9 @@
+<div class="menu">
+        <div class="menubutton"><i class="icon-cog-alt"></i></div>
+        <div class="menu-content"> 
+            <div>
+                <h1>TRYBY NAUKI</h1>
+                
+            </div>
+        </div>
+    </div>
